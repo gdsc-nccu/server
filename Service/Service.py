bc81e8f825e3a968cb7e49266ee1d723383181ce
Shoejust <141555665+JustinHsu1019@users.noreply.github.com>
2023-12-09 17:51:40 +0800

    Add Assign Router

--- Service/Service.py
+++ Service/Service.py
@@ -5,7 +5,7 @@ from google.cloud.firestore_v1.document import DocumentReference
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('server.json')
+cred = credentials.Certificate('/home/testforgdsc/mysite/server.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -32,6 +32,52 @@ def create_document():
     ref = db.collection(collection_name)
     ref.add(data)
     return jsonify({"message": "Document added successfully"}), 201
+
+@app.route('/assign_member_to_project', methods=['POST'])
+def assign_member_to_project():
+    user_email = request.json['user_email']
+    project_name = request.json['project_name']
+    
+    user_ref = db.collection('users').where('email', '==', user_email).limit(1).stream()
+    project_ref = db.collection('projects').where('name', '==', project_name).limit(1).stream()
+
+    user_doc = next(user_ref, None)
+    project_doc = next(project_ref, None)
+
+    if not user_doc or not project_doc:
+        return jsonify({"message": "User or Project not found"}), 404
+
+    project_doc.reference.update({
+        'team_members': firestore.ArrayUnion([user_doc.reference])
+    })
+    user_doc.reference.update({
+        'projects_involved': firestore.ArrayUnion([project_doc.reference])
+    })
+
+    return jsonify({"message": "Member assigned to project successfully"}), 200
+
+@app.route('/assign_pm', methods=['POST'])
+def assign_project_manager():
+    user_email = request.json['user_email']
+    project_name = request.json['project_name']
+    
+    user_ref = db.collection('users').where('email', '==', user_email).limit(1).stream()
+    project_ref = db.collection('projects').where('name', '==', project_name).limit(1).stream()
+
+    user_doc = next(user_ref, None)
+    project_doc = next(project_ref, None)
+
+    if not user_doc or not project_doc:
+        return jsonify({"message": "User or Project not found"}), 404
+
+    project_doc.reference.update({
+        'project_manager': user_doc.reference
+    })
+    user_doc.reference.update({
+        'managed_projects': firestore.ArrayUnion([project_doc.reference])
+    })
+
+    return jsonify({"message": "Project manager assigned successfully"}), 200
 
 @app.route('/read', methods=['GET'])
 def read_documents():
@@ -89,4 +135,4 @@ def delete_document(collection, doc_id):
     return jsonify({"message": "Document deleted successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
